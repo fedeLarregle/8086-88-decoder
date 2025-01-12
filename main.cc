9ec06021c588551d295ea61183672228ae43ccaa
Federico <fede.larregle@gmail.com>
2025-01-12 19:46:07 +0100

    Add support for signed displacements and explicit sizes

--- main.cc
+++ main.cc
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdint.h>
 
+int16_t abs(int16_t value) {
+    return value < 0 ? -value : value;
+}
+
+int8_t abs(int8_t value) {
+    return value < 0 ? -value : value;
+}
+
 const char * effective_addr_calculation[3][8] = {
     // MOD = 00
     [0][0] = "[bx + si]",      [0][1] = "[bx + di]",      [0][2] = "[bp + si]",      [0][3] = "[bp + di]",
     [0][4] = "[si]",           [0][5] = "[di]",           [0][6] = "[%d]",           [0][7] = "[bx]",
     // MOD = 01
-    [1][0] = "[bx + si + %d]", [1][1] = "[bx + di + %d]", [1][2] = "[bp + si + %d]", [1][3] = "[bp + di + %d]",
-    [1][4] = "[si + %d]",      [1][5] = "[di + %d]",      [1][6] = "[bp + %d]",      [1][7] = "[bx + %d]",
+    [1][0] = "[bx + si %c %d]", [1][1] = "[bx + di %c %d]", [1][2] = "[bp + si %c %d]", [1][3] = "[bp + di %c %d]",
+    [1][4] = "[si %c %d]",      [1][5] = "[di %c %d]",      [1][6] = "[bp %c %d]",      [1][7] = "[bx %c %d]",
     // MOD = 10
-    [2][0] = "[bx + si + %d]", [2][1] = "[bx + di + %d]", [2][2] = "[bp + si + %d]", [2][3] = "[bp + di + %d]",
-    [2][4] = "[si + %d]",      [2][5] = "[di + %d]",      [2][6] = "[bp + %d]",      [2][7] = "[bx + %d]"
+    [2][0] = "[bx + si %c %d]", [2][1] = "[bx + di %c %d]", [2][2] = "[bp + si %c %d]", [2][3] = "[bp + di %c %d]",
+    [2][4] = "[si %c %d]",      [2][5] = "[di %c %d]",      [2][6] = "[bp %c %d]",      [2][7] = "[bx %c %d]"
 };
 
 const char * reg_rm_11[2][8] = {
@@ -50,7 +58,7 @@ bool matches_mov_opcode(uint8_t byte, mov_mode *mov) {
     if (((byte >> 2) & 0b111111) == 0b100010) {
         *mov = rm_to_rm;
         result = true;
-    } else if (((byte >> 2) & 0b1111111) == 0b1100011) {
+    } else if (((byte >> 1) & 0b1111111) == 0b1100011) {
         *mov = imm_to_rm;
         result = true;
     } else if (((byte >> 4) & 0b1111) == 0b1011) {
@@ -73,6 +81,7 @@ bool matches_mov_opcode(uint8_t byte, mov_mode *mov) {
     return result;
 }
 
+// TODO(fede): Remove all these print hacks
 void print_mov(mov_opcode * opcode, uint16_t data) {
     const char * calc = effective_addr_calculation[opcode->mod][opcode->rm];
     const char * reg = reg_rm_11[opcode->w][opcode->reg];
@@ -82,7 +91,47 @@ void print_mov(mov_opcode * opcode, uint16_t data) {
         printf("\n");
     } else {
         printf("mov ");
-        printf(calc, ((uint8_t)(data >> 8)));
+        printf(calc, data);
+        printf(", %s\n", reg);
+    }
+}
+
+void print_mov(mov_opcode * opcode, int16_t data) {
+    const char * calc = effective_addr_calculation[opcode->mod][opcode->rm];
+    const char * reg = reg_rm_11[opcode->w][opcode->reg];
+    char sign = '+';
+    
+    if (data < 0) {
+        sign = '-';
+    }
+
+    if (opcode->d) {
+        printf("mov %s, ", reg);
+        printf(calc, sign, abs(data));
+        printf("\n");
+    } else {
+        printf("mov ");
+        printf(calc, sign, abs(data));
+        printf(", %s\n", reg);
+    }
+}
+
+void print_mov(mov_opcode * opcode, int8_t data) {
+    const char * calc = effective_addr_calculation[opcode->mod][opcode->rm];
+    const char * reg = reg_rm_11[opcode->w][opcode->reg];
+    char sign = '+';
+    
+    if (data < 0) {
+        sign = '-';
+    }
+    
+    if (opcode->d) {
+        printf("mov %s, ", reg);
+        printf(calc, sign, abs(data));
+        printf("\n");
+    } else {
+        printf("mov ");
+        printf(calc, sign, abs(data));
         printf(", %s\n", reg);
     }
 }
@@ -137,7 +186,7 @@ int main(int argc, char *argv[]) {
 
                                 opcode.disp_high = byte;
 
-                                print_mov(&opcode, (((uint16_t) opcode.disp_high << 8) | opcode.disp_low));
+                                print_mov(&opcode, (uint16_t)(((uint16_t) opcode.disp_high << 8) | opcode.disp_low));
                                 break;
                             }
 
@@ -157,7 +206,7 @@ int main(int argc, char *argv[]) {
 
                             opcode.disp_low = byte;
 
-                            print_mov(&opcode, (((uint16_t) opcode.disp_high << 8) | opcode.disp_low));
+                            print_mov(&opcode, (int8_t) opcode.disp_low);
                             break;
                         }
                         case 0b00000010: { /* 16 bit displacement */
@@ -171,7 +220,9 @@ int main(int argc, char *argv[]) {
 
                             opcode.disp_high = byte;
 
-                            print_mov(&opcode, (((uint16_t) opcode.disp_high << 8) | opcode.disp_low));
+                            int16_t value = ((int16_t) opcode.disp_high << 8) | opcode.disp_low;
+
+                            print_mov(&opcode, value);
                             break;
                         }
                         case 0b00000011: {
@@ -195,6 +246,250 @@ int main(int argc, char *argv[]) {
                     break;
                 }
                 case imm_to_rm: {
+                    opcode.w = (byte & 0b00000001);
+                    size_t read_byte = fread(&byte, 1, 1, file);
+                    if (!read_byte) { return 1; }
+                    opcode.mod = (byte >> 6 & 0b00000011);
+                    opcode.reg = (byte >> 3 & 0b00000111);
+                    opcode.rm  = (byte >> 0 & 0b00000111);
+
+                    switch (opcode.mod) {
+                        case 0b00000000: {
+                            char * rm_code;
+                            if (opcode.rm == 0b00000110) {
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+
+                                opcode.disp_low = byte;
+
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+
+                                opcode.disp_high = byte;
+
+                                if (opcode.w) {
+                                    read_byte = fread(&byte, 1, 1, file);
+                                    if (!read_byte) { return 1; }
+                                    opcode.data_low = byte;
+
+                                    read_byte = fread(&byte, 1, 1, file);
+                                    if (!read_byte) { return 1; }
+                                    opcode.data_high = byte;
+
+                                    const char * calc = effective_addr_calculation[opcode.mod][opcode.rm];
+                                    uint16_t disp_value = ((uint16_t) opcode.disp_high << 8) | opcode.disp_low;
+                                    uint16_t data_value = ((uint16_t) opcode.data_high << 8) | opcode.data_low;
+
+                                    if (opcode.d) {
+                                        printf("mov word %d, ", data_value);
+                                        printf(calc, '+', disp_value);
+                                        printf("\n");
+                                    } else {
+                                        printf("mov ");
+                                        printf(calc, '+', disp_value);
+                                        printf(", word %d\n", data_value);
+                                    }
+                                } else {
+                                    read_byte = fread(&byte, 1, 1, file);
+                                    if (!read_byte) { return 1; }
+                                    opcode.disp_low = byte;
+
+                                    const char * calc = effective_addr_calculation[opcode.mod][opcode.rm];
+                                    uint16_t disp_value = opcode.disp_low;
+                                    uint16_t data_value = opcode.data_low;
+
+                                    if (opcode.d) {
+                                        printf("mov byte %d, ", data_value);
+                                        printf(calc, '+', disp_value);
+                                        printf("\n");
+                                    } else {
+                                        printf("mov ");
+                                        printf(calc, '+', disp_value);
+                                        printf(", byte %d\n", data_value);
+                                    }
+                                }
+                                break;
+                            }
+
+                            if (opcode.w) {
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.data_low = byte;
+
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.data_high = byte;
+
+                                const char * calc = effective_addr_calculation[opcode.mod][opcode.rm];
+                                uint16_t data_value = ((uint16_t) opcode.data_high << 8) | opcode.data_low;
+
+                                if (opcode.d) {
+                                    printf("mov word %d, %s\n", data_value, calc);
+                                } else {
+                                    printf("mov %s, word %d\n", calc, data_value);
+                                }
+                            } else {
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.data_low = byte;
+                                
+                                const char * calc = effective_addr_calculation[opcode.mod][opcode.rm];
+
+                                if (opcode.d) {
+                                    printf("mov byte %d, %s\n", opcode.data_low, calc);
+                                } else {
+                                    printf("mov %s, byte %d\n", calc, opcode.data_low);
+                                }
+                            }
+
+                            break;
+                        }
+                        case 0b00000001: {  /* 8 bit displacement */
+                            read_byte = fread(&byte, 1, 1, file);
+                            if (!read_byte) { return 1; }
+
+                            opcode.disp_low = byte;
+
+                            if (opcode.w) {
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.data_low = byte;
+
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.data_high = byte;
+
+                                const char * calc = effective_addr_calculation[opcode.mod][opcode.rm];
+                                uint16_t disp_value = ((uint16_t) opcode.disp_high << 8) | opcode.disp_low;
+                                uint16_t data_value = ((uint16_t) opcode.data_high << 8) | opcode.data_low;
+
+                                if (opcode.d) {
+                                    printf("mov word %d, ", data_value);
+                                    printf(calc, '+', disp_value);
+                                    printf("\n");
+                                } else {
+                                    printf("mov ");
+                                    printf(calc, '+', disp_value);
+                                    printf(", word %d\n", data_value);
+                                }
+                            } else {
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.disp_low = byte;
+
+                                const char * calc = effective_addr_calculation[opcode.mod][opcode.rm];
+                                uint16_t disp_value = opcode.disp_low;
+                                uint16_t data_value = opcode.data_low;
+
+                                if (opcode.d) {
+                                    printf("mov byte %d, ", data_value);
+                                    printf(calc, '+', disp_value);
+                                    printf("\n");
+                                } else {
+                                    printf("mov ");
+                                    printf(calc, '+', disp_value);
+                                    printf(", byte %d\n", data_value);
+                                }
+                            }
+
+                            break;
+                        }
+                        case 0b00000010: { /* 16 bit displacement */
+                            read_byte = fread(&byte, 1, 1, file);
+                            if (!read_byte) { return 1; }
+
+                            opcode.disp_low = byte;
+
+                            read_byte = fread(&byte, 1, 1, file);
+                            if (!read_byte) { return 1; }
+
+                            opcode.disp_high = byte;
+
+                            if (opcode.w) {
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.data_low = byte;
+
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.data_high = byte;
+
+                                const char * calc = effective_addr_calculation[opcode.mod][opcode.rm];
+                                uint16_t disp_value = ((uint16_t) opcode.disp_high << 8) | opcode.disp_low;
+                                uint16_t data_value = ((uint16_t) opcode.data_high << 8) | opcode.data_low;
+
+                                if (opcode.d) {
+                                    printf("mov word %d, ", data_value);
+                                    printf(calc, '+', disp_value);
+                                    printf("\n");
+                                } else {
+                                    printf("mov ");
+                                    printf(calc, '+', disp_value);
+                                    printf(", word %d\n", data_value);
+                                }
+                            } else {
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.disp_low = byte;
+
+                                const char * calc = effective_addr_calculation[opcode.mod][opcode.rm];
+                                uint16_t disp_value = opcode.disp_low;
+                                uint16_t data_value = opcode.data_low;
+
+                                if (opcode.d) {
+                                    printf("mov byte %d, ", data_value);
+                                    printf(calc, '+', disp_value);
+                                    printf("\n");
+                                } else {
+                                    printf("mov ");
+                                    printf(calc, '+', disp_value);
+                                    printf(", byte %d\n", data_value);
+                                }
+                            }
+                            break;
+                        }
+                        case 0b00000011: {
+                            if (!opcode.d) {
+                                uint8_t tmp = opcode.reg;
+                                opcode.reg = opcode.rm;
+                                opcode.rm = tmp;
+                            }
+
+                            const char * rm_code  = reg_rm_11[opcode.w][(opcode.rm )];
+
+                            if (opcode.w) {
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.data_low = byte;
+
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.data_high = byte;
+
+                                uint16_t data_value = ((uint16_t) opcode.data_high << 8) | opcode.data_low;
+
+                                if (opcode.d) {
+                                    printf("mov word %d, %s\n", data_value, rm_code);
+                                } else {
+                                    printf("mov %s, word %d\n", rm_code, data_value);
+                                }
+                            } else {
+                                read_byte = fread(&byte, 1, 1, file);
+                                if (!read_byte) { return 1; }
+                                opcode.disp_low = byte;
+
+                                if (opcode.d) {
+                                    printf("mov byte %d, %s\n", opcode.data_low, rm_code);
+                                } else {
+                                    printf("mov %s, byte %d\n", rm_code, opcode.data_low);
+                                }
+                            }
+
+                            break;
+                        }
+
+                        default: { return 1; }
+                    }
                     break;
                 }
                 case imm_to_r: {
@@ -264,8 +559,14 @@ int main(int argc, char *argv[]) {
                     }
                     break;
                 }
-                case rm_to_seg: { break; }
-                case seg_to_rm: { break; }
+                case rm_to_seg: {
+                    printf("[DEBUG] REGISTER/MEMORY TO SEGMENT FOUND\n\n");
+                    break;
+                }
+                case seg_to_rm: {
+                    printf("[DEBUG] SEGMENT TO REGISTER/MEMORY FOUND\n\n");
+                    break;
+                }
                 default: {
                     fprintf(stderr, "[ERROR] Not supported MOV opcode mode: %u\n", opcode.mode);
                     return 1;
